87af118f62ec89d0708c02f8011f977acfa2446c
Ridl-00 <2190585675@qq.com>
2025-08-02 13:44:49 +0800

    Merge complete

--- myCPU/mycpu.v
+++ myCPU/mycpu.v
@@ -1,97 +1,5 @@
 `include "defines.v"
 
-<<<<<<< HEAD:myCPU/mycpu_top_toblock.v
-module core_top(
-    input      [7:0] intrpt,
-
-    input wire aclk,
-    input wire aresetn,
-
-//soc_axi
-    //ar
-    output wire [3:0] arid,
-    output wire [31:0] araddr,
-    output wire [7:0] arlen,
-    output wire [2:0] arsize,
-    output wire [1:0] arburst,
-    output wire [1:0] arlock,
-    output wire [3:0] arcache,
-    output wire [2:0] arprot,
-    output wire arvalid,
-    input wire arready,
-
-    //r
-    input wire [3:0] rid,
-    input wire [31:0] rdata,
-    input wire [1:0] rresp,
-    input wire rlast,
-    input wire rvalid,
-    output wire rready,
-
-    //aw
-    output wire [3:0] awid,
-    output wire [31:0] awaddr,
-    output wire [7:0] awlen,
-    output wire [2:0] awsize,
-    output wire [1:0] awburst,
-    output wire [1:0] awlock,
-    output wire [3:0] awcache,
-    output wire [2:0] awprot,
-    output wire awvalid,
-    input wire awready,
-
-    //w
-    output wire [3:0] wid,
-    output wire [31:0] wdata,
-    output wire [3:0] wstrb,
-    output wire wlast,
-    output wire wvalid,
-    input wire wready,
-
-    //b
-    input wire [3:0] bid,
-    input wire [1:0] bresp,
-    input wire bvalid,
-    output wire bready,
-
-// //soc_hs_bram
-//   //类SRAM
-//   output                      inst_sram_req,
-//   output                      inst_sram_wr,
-//   output [               3:0] inst_sram_wstrb,
-//   output [               1:0] inst_sram_size,
-//   output [              31:0] inst_sram_addr,
-//   output [              31:0] inst_sram_wdata,
-//   input [              31:0] inst_sram_rdata,
-//   input                      inst_sram_addr_ok,
-//   input                      inst_sram_data_ok,
-
-//   output                      data_sram_req,
-//   output                      data_sram_wr,
-//   output [               3:0] data_sram_wstrb,
-//   output [               1:0] data_sram_size,
-//   output [              31:0] data_sram_addr,
-//   output [              31:0] data_sram_wdata,
-//   input [              31:0] data_sram_rdata,
-//   input                      data_sram_addr_ok,
-//   input                      data_sram_data_ok,
-
-//debug interface
-    input  wire         break_point     ,        
-    input  wire         infor_flag      ,        
-    input  wire [4:0]   reg_num         ,        
-    input  wire         ws_valid        ,        
-    input  wire         rf_rdata        ,        
-
-    //debug0
-    output wire [31:0] debug0_wb_pc,
-    output wire [ 3:0] debug0_wb_rf_wen,
-    output wire [ 4:0] debug0_wb_rf_wnum,
-    output wire [31:0] debug0_wb_rf_wdata
-);
-
-
-=======
 module mycpu_core(
     input wire      aclk,
     input wire      aresetn,
@@ -123,7 +31,6 @@ module mycpu_core(
     output wire [31:0] debug_wb_rf_wdata
 );
 
->>>>>>> Haoyue:myCPU/mycpu.v
   wire                      id_allowin;
   wire                      exe_allowin;
   wire                      mem_allowin;
@@ -227,31 +134,6 @@ wire  [ 9:0]                   csr_asid_in     ;
 //general use
 wire [ 1:0]                   csr_plv_out      ;
 
-<<<<<<< HEAD:myCPU/mycpu_top_toblock.v
-
-  //类SRAM
-  wire                      inst_sram_req;
-  wire                      inst_sram_wr;
-  wire [               3:0] inst_sram_wstrb;
-  wire [               1:0] inst_sram_size;
-  wire [              31:0] inst_sram_addr;
-  wire [              31:0] inst_sram_wdata;
-  wire [              31:0] inst_sram_rdata;
-  wire                      inst_sram_addr_ok;
-  wire                      inst_sram_data_ok;
-  wire                      data_sram_req;
-  wire                      data_sram_wr;
-  wire [               3:0] data_sram_wstrb;
-  wire [               1:0] data_sram_size;
-  wire [              31:0] data_sram_addr;
-  wire [              31:0] data_sram_wdata;
-  wire [              31:0] data_sram_rdata;
-  wire                      data_sram_addr_ok;
-  wire                      data_sram_data_ok;
-
-
-=======
->>>>>>> Haoyue:myCPU/mycpu.v
   csr u_csr(
         .clk                (aclk),
         .resetn             (aresetn),
@@ -479,75 +361,75 @@ assign {
 
       .wb_to_csr_bus(wb_to_csr_bus),
 
-      .debug_wb_pc      (debug0_wb_pc),
-      .debug_wb_rf_we  (debug0_wb_rf_wen),
-      .debug_wb_rf_wnum (debug0_wb_rf_wnum),
-      .debug_wb_rf_wdata(debug0_wb_rf_wdata)
+      .debug_wb_pc      (debug_wb_pc),
+      .debug_wb_rf_we  (debug_wb_rf_we),
+      .debug_wb_rf_wnum (debug_wb_rf_wnum),
+      .debug_wb_rf_wdata(debug_wb_rf_wdata)
 
 );
 
   //axi
-  axi_bridge u_axi_bridge (
-      .clk              (aclk),
-      .aresetn          (aresetn),
-      .arid             (arid),
-      .araddr           (araddr),
-      .arlen            (arlen),
-      .arsize           (arsize),
-      .arburst          (arburst),
-      .arlock           (arlock),
-      .arcache          (arcache),
-      .arprot           (arprot),
-      .arvalid          (arvalid),
-      .arready          (arready),
-      .rid              (rid),
-      .rdata            (rdata),
-      .rresp            (rresp),
-      .rlast            (rlast),
-      .rvalid           (rvalid),
-      .rready           (rready),
-      .awid             (awid),
-      .awaddr           (awaddr),
-      .awlen            (awlen),
-      .awsize           (awsize),
-      .awburst          (awburst),
-      .awlock           (awlock),
-      .awcache          (awcache),
-      .awprot           (awprot),
-      .awvalid          (awvalid),
-      .awready          (awready),
-      .wid              (wid),
-      .wdata            (wdata),
-      .wstrb            (wstrb),
-      .wlast            (wlast),
-      .wvalid           (wvalid),
-      .wready           (wready),
-      .bid              (bid),
-      .bresp            (bresp),
-      .bvalid           (bvalid),
-      .bready           (bready),
+  // axi_bridge u_axi_bridge (
+  //     .clk              (aclk),
+  //     .aresetn          (aresetn),
+  //     .arid             (arid),
+  //     .araddr           (araddr),
+  //     .arlen            (arlen),
+  //     .arsize           (arsize),
+  //     .arburst          (arburst),
+  //     .arlock           (arlock),
+  //     .arcache          (arcache),
+  //     .arprot           (arprot),
+  //     .arvalid          (arvalid),
+  //     .arready          (arready),
+  //     .rid              (rid),
+  //     .rdata            (rdata),
+  //     .rresp            (rresp),
+  //     .rlast            (rlast),
+  //     .rvalid           (rvalid),
+  //     .rready           (rready),
+  //     .awid             (awid),
+  //     .awaddr           (awaddr),
+  //     .awlen            (awlen),
+  //     .awsize           (awsize),
+  //     .awburst          (awburst),
+  //     .awlock           (awlock),
+  //     .awcache          (awcache),
+  //     .awprot           (awprot),
+  //     .awvalid          (awvalid),
+  //     .awready          (awready),
+  //     .wid              (wid),
+  //     .wdata            (wdata),
+  //     .wstrb            (wstrb),
+  //     .wlast            (wlast),
+  //     .wvalid           (wvalid),
+  //     .wready           (wready),
+  //     .bid              (bid),
+  //     .bresp            (bresp),
+  //     .bvalid           (bvalid),
+  //     .bready           (bready),
 
 
-      .inst_sram_req    (inst_sram_req),
-      .inst_sram_wr     (inst_sram_wr),
-      .inst_sram_size   (inst_sram_size),
-      .inst_sram_wstrb  (inst_sram_wstrb),
-      .inst_sram_addr   (inst_sram_addr),
-      .inst_sram_wdata  (inst_sram_wdata),
-      .inst_sram_addr_ok(inst_sram_addr_ok),
-      .inst_sram_data_ok(inst_sram_data_ok),
-      .inst_sram_rdata  (inst_sram_rdata),
+  //     .inst_sram_req    (inst_sram_req),
+  //     .inst_sram_wr     (inst_sram_wr),
+  //     .inst_sram_size   (inst_sram_size),
+  //     .inst_sram_wstrb  (inst_sram_wstrb),
+  //     .inst_sram_addr   (inst_sram_addr),
+  //     .inst_sram_wdata  (inst_sram_wdata),
+  //     .inst_sram_addr_ok(inst_sram_addr_ok),
+  //     .inst_sram_data_ok(inst_sram_data_ok),
+  //     .inst_sram_rdata  (inst_sram_rdata),
     
-      .data_sram_req    (data_sram_req),
-      .data_sram_wr     (data_sram_wr),
-      .data_sram_size   (data_sram_size),
-      .data_sram_wstrb   (data_sram_wstrb),
-      .data_sram_addr   (data_sram_addr),
-      .data_sram_wdata  (data_sram_wdata),
-      .data_sram_addr_ok(data_sram_addr_ok),
-      .data_sram_data_ok(data_sram_data_ok),
-      .data_sram_rdata  (data_sram_rdata)
-  );
+  //     .data_sram_req    (data_sram_req),
+  //     .data_sram_wr     (data_sram_wr),
+  //     .data_sram_size   (data_sram_size),
+  //     .data_sram_wstrb   (data_sram_wstrb),
+  //     .data_sram_addr   (data_sram_addr),
+  //     .data_sram_wdata  (data_sram_wdata),
+  //     .data_sram_addr_ok(data_sram_addr_ok),
+  //     .data_sram_data_ok(data_sram_data_ok),
+  //     .data_sram_rdata  (data_sram_rdata)
+  // );
 
 
 endmodule
